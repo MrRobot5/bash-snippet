fd0c3ba34ae71edb2be5aa6a4c61c0c5771f06fb
MrRobot5 <1050335971@qq.com>
2023-12-13 17:09:48 +0800

    docs: HelloGithub_目录 👍

--- mrrobot-git-update.sh
+++ mrrobot-git-update.sh
@@ -1,35 +1,39 @@
 
 cd /d/workspace/MrRobot5.github.io
 pwd
-gpp
+git pull; git push
 
 #cd /d/workspace-experiment/quotation-calculator
 #pwd
-#gpp 
+#git pull; git push 
+
+cd /d/export/RL_strategy
+pwd
+git pull; git push
 
 cd /d/github/java-snippet
 pwd
-gpp 
+git pull; git push
 
 cd /d/github/bash-snippet
 pwd
-gpp 
+git pull; git push
 
 #cd /d/workspace-experiment/mybatis-sample
 #pwd
-#gpp
+#git pull; git push
 
 #cd /d/软件/idea
 #pwd
-#gpp
+#git pull; git push
 
 cd /d/github/python-snippet
 pwd
-gpp
+git pull; git push
 
 cd /d/workspace/javascript-snippet
 pwd
-gpp
+git pull; git push
 
 
 echo 'sync success! await 1min ...'
